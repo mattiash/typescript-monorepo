b022abe64b0bd448b3e50e609fbc7c313a8f11af
Mattias Holmlund <mattias@holmlund.se>
2025-12-14 14:55:16 +0100

    Import from common/utils/file2

--- service-a/src/test/test-basic.ts
+++ service-a/src/test/test-basic.ts
@@ -1,5 +1,6 @@
 import { test } from "purple-tape";
 import { double, trueish } from "../lib/basic";
+import { file2 } from "@tsmr/common/utils/file2";
 
 test("Basic A", (t) => {
   t.true(trueish, "trueish works");
@@ -8,4 +9,8 @@ test("Basic A", (t) => {
 
 test("Double", (t) => {
   t.equal(double(2), 4);
+});
+
+test("Import from @tsmr/common/utils/file2", (t) => {
+  t.equal(file2, "Hello");
 });
