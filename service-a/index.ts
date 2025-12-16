4fdaf767ec02901c0d6390b73a602f305c9f3dce
Mattias Holmlund <mattias@holmlund.se>
2025-12-16 19:33:46 +0100

    Avoid src/ in service-a

--- service-a/index.ts
+++ service-a/index.ts
@@ -2,6 +2,8 @@ import "@tsmr/common";
 import colors from "yoctocolors";
 import { emojify } from "node-emoji";
 import { file2 } from "@tsmr/common/utils/file2";
+import { double } from "./lib/basic";
 
 console.log(emojify(":unicorn: ") + colors.green("service-a/index.ts"));
 console.log(file2);
+console.log("double(2)", double(2));
